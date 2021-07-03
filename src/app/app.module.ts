15d4cf86fd555d69819fb8a16a191f05b6de7181
jinwu09 <bjaj09yahoo.com@gmail.com>
2021-07-03 17:37:13 +0800

    still in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { HeaderComponent } from './components/header/header.component';
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> 7738e061a722b8406a5d2748d61558b94fe12eba
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     HeaderComponent
+=======
+    LoginComponent
+>>>>>>> 7738e061a722b8406a5d2748d61558b94fe12eba
   ],
   imports: [
     BrowserModule,
